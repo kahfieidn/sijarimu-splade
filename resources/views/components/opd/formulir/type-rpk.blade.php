b8af4f5f400f97320d81f743a1b03c41f3ab4c9b
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2024-01-24 09:13:08 +0700

    update

--- resources/views/components/opd/formulir/type-rpk.blade.php
+++ resources/views/components/opd/formulir/type-rpk.blade.php
@@ -143,6 +143,7 @@
     </div>
 </div>
 
+
 <div class="p-4 sm:ml-64">
     <div class="bg-white p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
         <h1 class="mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Tindak <span class="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">Lanjut</span></h1>
