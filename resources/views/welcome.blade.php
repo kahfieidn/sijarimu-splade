7b411759b7e4989a7074252f93e63ba94fed5069
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2024-02-05 16:01:05 +0700

    fixed welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,5 @@
 <x-guest-layout>
 
-
-
-
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -37,30 +34,32 @@
         </div>
     </nav>
 
+    <div class="bg-gradient-to-b from-blue-50 to-transparent">
 
-
-    <section class="h-screen bg-white dark:bg-gray-900">
-        <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
-            <div class="flex flex-col justify-center">
-                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Selamat Datang di Aplikasi Sijarimu</h1>
-                <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Aplikasi Perizinan Online Non OSS DPMPTSP Provinsi Kepulauan Riau.</p>
-                <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
-                    <Link href="/login" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+        <section class=" dark:from-blue-900 dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
+            <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
+                <div class="flex flex-col justify-center">
+                    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Selamat Datang di Aplikasi Sijarimu</h1>
+                    <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Aplikasi Perizinan Online Non OSS di Kelola oleh DPMPTSP Provinsi Kepulauan Riau.</p>
+                    <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
+                        <Link href="/login" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                         Login
                         <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
-                    </Link>
-                    <Link href="/register" class="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                        </Link>
+                        <Link href="/register" class="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                         Daftar Sekarang
-                    </Link>
+                        </Link>
+                    </div>
+                </div>
+                <div class="flex flex-col justify-center">
+                    <iframe class="mx-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl" src="https://www.youtube.com/embed/fUd1Nfsii5U?si=Be5OzAh60tBguvZM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
-            <div class="flex flex-col justify-center">
-                <iframe class="mx-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl" src="https://www.youtube.com/embed/fUd1Nfsii5U?si=Be5OzAh60tBguvZM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-        </div>
-    </section>
+        </section>
+    </div>
+
 
 
 
