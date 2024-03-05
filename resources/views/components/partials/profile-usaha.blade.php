99f4740005c606a5a38f53db903c51b9bdb19aac
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2024-03-05 14:24:47 +0700

    fixed vue warn at profile usaha

--- resources/views/components/partials/profile-usaha.blade.php
+++ resources/views/components/partials/profile-usaha.blade.php
@@ -3,33 +3,25 @@
         <h1 class="mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Informasi <span class="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">Profile Usaha</span></h1>
         <div class="grid md:grid-cols-2 md:gap-6">
             <div class="relative z-0 w-full mb-6 group">
-                <x-splade-input required disabled name="profile.npwp" type="text" placeholder="NPWP" label="NPWP" />
+                <x-splade-input required readonly name="profile.npwp" type="text" placeholder="NPWP" label="NPWP" />
             </div>
             <div class="relative z-0 w-full mb-6 group">
-                <x-splade-input required disabled name="profile.perusahaan" type="text" placeholder="Perusahaan" label="Perusahaan" />
+                <x-splade-input required readonly name="profile.perusahaan" type="text" placeholder="Perusahaan" label="Perusahaan" />
             </div>
         </div>
         <div class="grid md:grid-cols-1 md:gap-6">
             <div class="relative z-0 w-full mb-6 group">
-                <x-splade-textarea required disabled name="profile.alamat" type="text" placeholder="Alamat perusahaan" label="Alamat perusahaan" autosize />
+                <x-splade-textarea required readonly name="profile.alamat" type="text" placeholder="Alamat perusahaan" label="Alamat perusahaan" autosize />
             </div>
         </div>
         <div class="grid md:grid-cols-2 md:gap-6">
             <div class="relative z-0 w-full mb-6 group">
-                <x-splade-select disabled name="profile.domisili" type="text" placeholder="Domisili" label="Domisili" required>
-                    <option value="Kabupaten Bintan">Kabupaten Bintan</option>
-                    <option value="Kabupaten Karimun">Kabupaten Karimun</option>
-                    <option value="Kabupaten Kepulauan Anambas">Kabupaten Kepulauan Anambas</option>
-                    <option value="Kabupaten Lingga">Kabupaten Lingga</option>
-                    <option value="Kabupaten Natuna">Kabupaten Natuna</option>
-                    <option value="Kota Batam">Kota Batam</option>
-                    <option value="Kota Tanjungpinang">Kota Tanjungpinang</option>
-                </x-splade-select>
+                <x-splade-input readonly name="profile.domisili" type="text" placeholder="Domisili" label="Domisili" required>
+                </x-splade-input>
             </div>
             <div class="relative z-0 w-full mb-6 group">
-                <x-splade-select disabled name="profile.provinsi_domisili" type="text" placeholder="Provinsi" label="Provinsi" required>
-                    <option value="Kepulauan Riau">Kepulauan Riau</option>
-                </x-splade-select>
+                <x-splade-input readonly name="profile.provinsi_domisili" type="text" placeholder="Provinsi" label="Provinsi" required>
+                </x-splade-input>
             </div>
         </div>
     </div>
