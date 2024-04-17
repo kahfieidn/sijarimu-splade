85cc2fc82deb47daa95c33b7930334808c12c438
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2024-04-17 07:56:10 +0700

    change interface bantuan

--- resources/views/bantuan/index.blade.php
+++ resources/views/bantuan/index.blade.php
@@ -6,16 +6,14 @@
     <div class="p-4 h-full sm:ml-64">
         <div class="bg-white p-8 border-dashed rounded-lg dark:border-gray-700">
 
+
             <section class="bg-white dark:bg-gray-900">
-                <div class="mx-auto max-w-screen-xl text-center lg:py-10 lg:px-10">
-                    <h1 class="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">HelpDesk Front Office DPMPTSP Provinsi Kepulauan Riau</h1>
-                    <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                        <a target="__blank" type="button" href="https://wa.me/6282216054080" class="text-white bg-[#128C7E] hover:bg-[#128C7E]/90 focus:ring-4 focus:outline-none focus:ring-[#128C7E]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2">
-                            <svg class="w-6 h-6 text-white-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M7.978 4a2.553 2.553 0 0 0-1.926.877C4.233 6.7 3.699 8.751 4.153 10.814c.44 1.995 1.778 3.893 3.456 5.572 1.68 1.679 3.577 3.018 5.57 3.459 2.062.456 4.115-.073 5.94-1.885a2.556 2.556 0 0 0 .001-3.861l-1.21-1.21a2.689 2.689 0 0 0-3.802 0l-.617.618a.806.806 0 0 1-1.14 0l-1.854-1.855a.807.807 0 0 1 0-1.14l.618-.62a2.692 2.692 0 0 0 0-3.803l-1.21-1.211A2.555 2.555 0 0 0 7.978 4Z" />
-                            </svg>
-                            WhatsApp : Fenti (Non Perizinan)
-                        </a>
+                <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+                    <div class="mx-auto max-w-screen-sm text-center">
+                        <h2 class="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">HelpDesk Front Office DPMPTSP Provinsi Kepulauan Riau</h2>
+                        <p class="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">Punya pertanyaan lain? Silahkan hubungi kami melalui jalur komunikasi berikut.</p>
+                        <a target="_blank" href="https://wa.me/6282216054080" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                            WhatsApp : Fenti (Non Perizinan)</a>
                     </div>
                 </div>
             </section>
