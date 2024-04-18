42a580db1e8f7399bd72b4abc6f9d35337cd52b4
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2024-04-18 11:33:45 +0700

    change workf

--- app/Http/Controllers/Bantuan/DashboardController.php
+++ app/Http/Controllers/Bantuan/DashboardController.php
@@ -7,9 +7,6 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return view('bantuan.index');
